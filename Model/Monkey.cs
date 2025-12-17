d4798138b9a0d495b3243fcec5854679c67d4f08
1hexcode <np01cp4a230335@islingtoncollege.edu.np>
2025-12-17 19:46:30 +0545

    add everything

---
+++ Model/Monkey.cs
@@ -0,0 +1,8 @@
+namespace MauiApp1.Model;
+
+public class Monkey
+{
+    public string Name {get; set;}
+    public string Location {get; set;}
+    public string Image {get; set;}
+}
---
+++ Model/Product.cs
@@ -0,0 +1,10 @@
+namespace MauiApp1.Model;
+
+public class Product
+{
+    public string ImageUrl { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public int Likes { get; set; }
+    public string Location { get; set; }
+}
