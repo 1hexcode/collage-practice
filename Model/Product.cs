fd0c828dcbaad6be0e398fa58626cc93c377918d
1hexcode <np01cp4a230335@islingtoncollege.edu.np>
2025-12-24 09:24:57 +0545

    edit from

--- Model/Product.cs
+++ Model/Product.cs
@@ -2,9 +2,22 @@ namespace MauiApp1.Model;
 
 public class Product
 {
+    [Required]
+    public int Id {get; set;}
+    [Required]
     public string ImageUrl { get; set; }
+    [Required]
+    [StringLength(50)]
     public string Name { get; set; }
+    [Required]
+    [Range(0, double.MaxValue, ErrrorMessage="The price must be greater than or equal to 0.")]
     public double Price { get; set; }
     public int Likes { get; set; }
     public string Location { get; set; }
+    [Required]
+    [Range(1,5)]
+    public int Rating { get; set; }
+    [Required]
+    [StringLength(100)]
+    public string Description { get; set; }
 }
--- obj/Debug/net10.0-ios/iossimulator-arm64/MauiApp1.AssemblyInfo.cs
+++ obj/Debug/net10.0-ios/iossimulator-arm64/MauiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d4798138b9a0d495b3243fcec5854679c67d4f08")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
